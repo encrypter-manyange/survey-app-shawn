c3a74a7b6a633294bf08a7d624ca4a75e886ab9c
Shaun <idevelop4mobile@gmail.com>
2018-11-12 15:23:07 +0200

    Updated content

--- src/App.js
+++ src/App.js
@@ -19,238 +19,210 @@ class App extends Component {
       quizzes: [
 
         {
-          name: 'HTML',
-          image: require("./images/html.jpg") ,
-          description:'HTMLs stands for HyperText Markup Language, It defines the structure of a Web document by using a variety of tags and attributes.',
+          name: 'Child Birth',
+          image: require("./images/preg.jpg") ,
+          description:'Quizzes about child birth',
           subQuiz: [
             {
-              name: 'Quiz 1', questions: '3', time: '30 sec', score: false,
+              name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
               qArr: [
                 {
-                  question: "What is use for image insertion?",
-                  option1: "img",
-                  option2: "h1",
-                  option3: "none",
-                  option4: "body",
+                  question: "What age can someone fall pregnant?",
+                  option1: "It depends on the person.",
+                  option2: "18",
+                  option3: "15",
+                  option4: "30",
                   answer: "1"
                 },
                 {
-                  question: "To underline the text?",
-                  option1: "u",
-                  option2: "underline",
-                  option3: "style",
-                  option4: "none",
-                  answer: "1"
-                },
-                {
-
-                  question: "HTML stands for?",
-                  option1: "HYPER TEXT MARKUP LANGUAGE'",
-                  option2: "HTPER TEXT TELESCOPR",
-                  option3: "HELP TEXT TERMINAL LANGUAGE",
-                  option4: "NONE",
-                  answer: "1"
-                },
-
-              ]
-            },
-            {
-              name: 'Quiz 2', questions: '3', time: '30 sec', score: false,
-              qArr: [
-                {
-                  question: "HTML use for?",
-                  option1: "structure",
-                  option2: "styling",
-                  option3: "scripting",
-                  option4: "none",
-                  answer: "1"
-                },
-                {
-                  question: "To underline the text?",
-                  option1: "u",
-                  option2: "underline",
-                  option3: "style",
-                  option4: "none",
-                  answer: "1"
-                },
-                {
-
-                  question: "HTML stands for?",
-                  option1: "HYPER TEXT MARKUP LANGUAGE'",
-                  option2: "HTPER TEXT TELESCOPR",
-                  option3: "HELP TEXT TERMINAL LANGUAGE",
-                  option4: "NONE",
-                  answer: "1"
-                }
-              ]
-            },
-          ]
-        },
-        {
-          name: 'CSS',
-          image: require("./images/css.jpg") ,
-          description:'CSS stands for Cascading Style Sheets, It describes how HTML elements are to be displayed on screen, paper, or in other media.',
-          subQuiz: [
-            {
-              name: 'Quiz 1', questions: '6', time: '30 sec', score: false,
-              qArr: [
-                {
-                  question: "Css stands for?",
-                  option1: "cascading style sheet'",
-                  option2: "cascade sheet style",
-                  option3: "color style sheet",
-                  option4: "contrast style sheet",
-                  answer: "1"
-                },
-                {
-                  question: "to change the color we use",
-                  option1: "color:",
-                  option2: "setColor:",
-                  option3: "color-style",
-                  option4: "change-color",
-                  answer: "1"
-                },
-                {
-                  question: "How to link css?",
-                  option1: "using link",
-                  option2: "using button",
-                  option3: "using javascript",
-                  option4: "using img",
-                  answer: "1"
-                },
-                {
-                  question: "To change the color we use?",
-                  option1: "color:",
-                  option2: "setColor:",
-                  option3: "color-style",
-                  option4: "change-color",
-                  answer: "1"
-                },
-                {
-                  question: "How to link css?",
-                  option1: "using link",
-                  option2: "using button",
-                  option3: "using javascript",
-                  option4: "using img",
-                  answer: "1"
-                },
-                {
-                  question: "How to link css?",
-                  option1: "using link",
-                  option2: "using button",
-                  option3: "using javascript",
-                  option4: "using img",
-                  answer: "1"
-                },
-
-              ]
-            },
-          ]
-        },
-        {
-          name: 'JavaScript',
-          image:  require("./images/js3.jpg") ,
-          description:' JavaScript is a "client-side" programming language. JavaScript scripts are read, interpreted and executed in your Web browser.',
-          subQuiz: [
-            {
-              name: 'Quiz 1', questions: '4', time: '30 sec', score: false,
-              qArr: [
-                {
-                  question: "How to create alert box?",
-                  option1: "alert='hello world'",
-                  option2: "aler('hello world')",
-                  option3: "alert.('hello world')",
-                  option4: "alert('hello world')",
+                  question: "Can I start having sex at any age?",
+                  option1: "No",
+                  option2: "If you would like",
+                  option3: "Ask your parents",
+                  option4: "If you are ready.",
                   answer: "4"
                 },
                 {
 
-                  question: "How to initialize variable?",
-                  option1: "variable name = 'ali'",
-                  option2: "var name = 'ali'",
-                  option3: "variable: 'ali'",
-                  option4: "variable. 'ali'",
-                  answer: "2"
-                },
-                {
-
-                  question: "How to push value in array?",
-                  option1: "arr.push(value)",
-                  option2: "arr.push.value",
-                  option3: "arr.(value)",
-                  option4: "arr.value.push(value)",
-                  answer: "1"
-                },
-                {
-
-                  question: "What is javascript?",
-                  option1: "programming language",
-                  option2: "scripting language",
-                  option3: "coding language",
-                  option4: "web language",
-                  answer: "2"
-                },
-              ]
-            },
-            {
-              name: 'Quiz 2', questions: '6', time: '30 sec', score: false,
-              qArr: [
-                {
-
-                  question: "Which creates random number?",
-                  option1: "Math.floor()",
-                  option2: "Math.ceil()",
-                  option3: "Math.random()",
-                  option4: "Math.high",
+                  question: "How do I know I am ready to have sex?",
+                  option1: "Your partner will tell you",
+                  option2: "Your body changes",
+                  option3: "There's no way of telling",
+                  option4: "When you start to feel like it",
                   answer: "3"
                 },
-                {
 
-                  question: "What does array returns?",
-                  option1: "function",
-                  option2: "object",
-                  option3: "index number",
-                  option4: "data type",
-                  answer: "2"
-                },
-                {
-
-                  question: "Object property name & value are separated by?",
-                  option1: "semicolon ;",
-                  option2: "colon :",
-                  option3: "dot .",
-                  option4: "comma ,",
-                  answer: "2"
-                },
-                {
-
-                  question: "Object properties are separated by?",
-                  option1: "comma ,",
-                  option2: "dot .",
-                  option3: "colon :",
-                  option4: "semicolon ;",
-                  answer: "1"
-                },
-                {
-
-                  question: "What is the correct syntax for object initialization?",
-                  option1: "var var_name = {property_name:property_value}",
-                  option2: "var var_name = {property_value:property_name}",
-                  option3: "var var_value = {property_name:property_value}",
-                  option4: "object = {property_name:property_value}",
-                  answer: "1"
-                },
-                {
-
-                  question: "DOM stands for?",
-                  option1: "document object modification",
-                  option2: "document observed module",
-                  option3: "document object model",
-                  option4: "document object module",
-                  answer: "3"
-                },
               ]
             },
+              {
+                  name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
+                  qArr: [
+                      {
+                          question: "What age can someone fall pregnant?",
+                          option1: "It depends on the person.",
+                          option2: "18",
+                          option3: "15",
+                          option4: "30",
+                          answer: "1"
+                      },
+                      {
+                          question: "Can I start having sex at any age?",
+                          option1: "No",
+                          option2: "If you would like",
+                          option3: "Ask your parents",
+                          option4: "If you are ready.",
+                          answer: "4"
+                      },
+                      {
+
+                          question: "How do I know I am ready to have sex?",
+                          option1: "Your partner will tell you",
+                          option2: "Your body changes",
+                          option3: "There's no way of telling",
+                          option4: "When you start to feel like it",
+                          answer: "3"
+                      },
+
+                  ]
+              },
+          ]
+        },
+        {
+          name: 'Contraception',
+          image: require("./images/contra.jpg") ,
+          description:'Test your knowledge about contraception and safe sex.',
+          subQuiz: [
+              {
+                  name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
+                  qArr: [
+                      {
+                          question: "What age can someone fall pregnant?",
+                          option1: "It depends on the person.",
+                          option2: "18",
+                          option3: "15",
+                          option4: "30",
+                          answer: "1"
+                      },
+                      {
+                          question: "Can I start having sex at any age?",
+                          option1: "No",
+                          option2: "If you would like",
+                          option3: "Ask your parents",
+                          option4: "If you are ready.",
+                          answer: "4"
+                      },
+                      {
+
+                          question: "How do I know I am ready to have sex?",
+                          option1: "Your partner will tell you",
+                          option2: "Your body changes",
+                          option3: "There's no way of telling",
+                          option4: "When you start to feel like it",
+                          answer: "3"
+                      },
+
+                  ]
+              },
+              {
+                  name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
+                  qArr: [
+                      {
+                          question: "What age can someone fall pregnant?",
+                          option1: "It depends on the person.",
+                          option2: "18",
+                          option3: "15",
+                          option4: "30",
+                          answer: "1"
+                      },
+                      {
+                          question: "Can I start having sex at any age?",
+                          option1: "No",
+                          option2: "If you would like",
+                          option3: "Ask your parents",
+                          option4: "If you are ready.",
+                          answer: "4"
+                      },
+                      {
+
+                          question: "How do I know I am ready to have sex?",
+                          option1: "Your partner will tell you",
+                          option2: "Your body changes",
+                          option3: "There's no way of telling",
+                          option4: "When you start to feel like it",
+                          answer: "3"
+                      },
+
+                  ]
+              },
+          ]
+        },
+        {
+          name: 'Childrens Rights',
+          image:  require("./images/rights.jpg") ,
+          description:' Test your knowledge about childrens rights.',
+          subQuiz: [
+              {
+                  name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
+                  qArr: [
+                      {
+                          question: "What age can someone fall pregnant?",
+                          option1: "It depends on the person.",
+                          option2: "18",
+                          option3: "15",
+                          option4: "30",
+                          answer: "1"
+                      },
+                      {
+                          question: "Can I start having sex at any age?",
+                          option1: "No",
+                          option2: "If you would like",
+                          option3: "Ask your parents",
+                          option4: "If you are ready.",
+                          answer: "4"
+                      },
+                      {
+
+                          question: "How do I know I am ready to have sex?",
+                          option1: "Your partner will tell you",
+                          option2: "Your body changes",
+                          option3: "There's no way of telling",
+                          option4: "When you start to feel like it",
+                          answer: "3"
+                      },
+
+                  ]
+              },
+              {
+                  name: 'Teenage Pregrancy', questions: '3', time: '30 sec', score: false,
+                  qArr: [
+                      {
+                          question: "What age can someone fall pregnant?",
+                          option1: "It depends on the person.",
+                          option2: "18",
+                          option3: "15",
+                          option4: "30",
+                          answer: "1"
+                      },
+                      {
+                          question: "Can I start having sex at any age?",
+                          option1: "No",
+                          option2: "If you would like",
+                          option3: "Ask your parents",
+                          option4: "If you are ready.",
+                          answer: "4"
+                      },
+                      {
+
+                          question: "How do I know I am ready to have sex?",
+                          option1: "Your partner will tell you",
+                          option2: "Your body changes",
+                          option3: "There's no way of telling",
+                          option4: "When you start to feel like it",
+                          answer: "3"
+                      },
+
+                  ]
+              },
           ]
         },
 
